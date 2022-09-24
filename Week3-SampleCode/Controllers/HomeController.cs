756c556907730b10d5154fda41922306e1826c9e
FolusoFayomi <foluso.fayomi@tri-c.edu>
2022-09-24 16:22:12 -0400

    Add files via upload

---
+++ Week3-SampleCode/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using AddMulTwoNum.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AddMulTwoNum.Controllers
+{
+   public class HomeController : Controller
+   {
+      //private readonly ILogger<HomeController> _logger;
+
+      //public HomeController(ILogger<HomeController> logger)
+      //{
+      //   _logger = logger;
+      //}
+      [HttpGet]
+      public IActionResult Index()
+      {
+         ViewBag.Sum = 0;
+         ViewBag.Product = 0;
+         return View();
+      }
+      [HttpPost]
+      public IActionResult Index(AddMulTwoNumModel val)
+      {
+         if (ModelState.IsValid)
+         {
+            ViewBag.Sum = val.AddTwoNum();
+            ViewBag.Product = val.MulTwoNum();
+         }
+         else
+         {
+            ViewBag.Sum = 0;
+            ViewBag.Product = 0;
+         }
+        
+         return View(val);
+      }
+         //public IActionResult Privacy()
+         //{
+         //   return View();
+         //}
+
+         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+         //public IActionResult Error()
+         //{
+         //   return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+         //}
+      }
+   }
---
+++ Week3-SampleCode/Models/AddMulTwoNumModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+//using System.Runtime.ExceptionServices;
+
+namespace AddMulTwoNum.Models
+{
+    public class AddMulTwoNumModel
+    {
+      [Required(ErrorMessage ="Please enter first number.")]
+      [Range(0, Double.MaxValue, ErrorMessage =
+         "First Number must be greater than 0")]
+      public double? Num1 { get; set; }
+
+      [Required(ErrorMessage = "Please enter second number.")]
+      [Range(0, Double.MaxValue, ErrorMessage =
+        "Second Number must be greater than 0")]
+      public double Num2 { get; set; }
+      public double? AddTwoNum()
+      {
+         double? Sum;
+         Sum = Num1 + Num2;
+         return Sum;
+      }
+
+      public double? MulTwoNum()
+      {
+         double? Product;
+         Product = Num1 * Num2;
+         return Product;
+      }
+   }
+}
